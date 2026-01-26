a42af95e56570fc1a45f06d9da3a0225533b1ae2
Anshul Verma <av4862187@gmail.com>
2026-01-26 22:50:45 +0530

    user route added

---
+++ server/Routes/userRoutes.js
@@ -0,0 +1,37 @@
+const express=require("express");
+const router=express.Router();
+const User=require("../Models/userModel");
+
+// to register new user
+router.post("/create",async(req,res)=>{
+    try{
+        const {name,email,password}=req.body;
+        const existuser=await User.findOne({email});
+        if(existuser){
+            return res.status(400).json({message:"User already registered with this email"});
+        }
+        await User.create({name,email,password});
+        return res.status(201).json({message:"User registered successfully.."});
+    }
+    catch(error){
+        return res.status(500).json({message:'Internal server error..'});
+    }
+})
+
+// to login user
+router.get("/login",async(req,res)=>{
+    try{
+        const {email,password}=req.body;
+        const existuser=await User.findOne({email});
+        if(!existuser || existuser.password!=password){
+            return res.status(400).json({message:"Invalid credentials"});
+        }
+
+        return res.status(201).json({message:"User logged in successfully.."});
+    }
+    catch(error){
+        return res.status(500).json({message:'Internal server error..'});
+    }
+})
+
+module.exports=router;
--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 const express=require("express");
 const connectDB = require("./config/db");
+const cors=require("cors");
+const UserRoute=require("./Routes/userRoutes");
 const app=express();
 app.use(express.json());
+app.use(cors());
 connectDB();
 
 app.get('/',(req,res)=>{
     res.send("hello world");
 })
-app.listen(3000,()=>{
-    console.log(`Server is running on port ${3000}`);
+app.use("/api/user",UserRoute);
+app.listen(5000,()=>{
+    console.log(`Server is running on port ${5000}`);
 })
